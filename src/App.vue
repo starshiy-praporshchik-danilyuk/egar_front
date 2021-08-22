9b8cd595d1384b6dba3bcd9e1e7699d43ec9ee23
Danil <Danil@DESKTOP-OPOG935>
2021-08-23 02:07:41 +0400

    фронт 0.3

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,12 @@
 
 <script>
 import LessonsList from './components/lessons-list.vue'
-//import axios from "axios";
 
 export default {
   name: 'App',
   components: {
     LessonsList
   },
-  /*data: function() {
-    return {
-      lessons: []
-  };
-},
-  created() {
-    axios.get('http://localhost:8081/teacher/lessons')
-        .then(response => this.lessons = response.data);
-  }*/
 }
 </script>
 
